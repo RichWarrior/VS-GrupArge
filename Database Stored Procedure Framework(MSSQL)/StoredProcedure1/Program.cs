05a16b607468fde53af7e9baa7e648b12fcd3457
Ömer Faruk Şahin <faruk_thecno@hotmail.com>
2018-07-02 10:02:43 +0300

    VS-GrupArge

--- Database Stored Procedure Framework(MSSQL)/StoredProcedure1/Program.cs
+++ Database Stored Procedure Framework(MSSQL)/StoredProcedure1/Program.cs
@@ -1,5 +1,4 @@
 ﻿using StoredProcedure1._Class;
-using StoredProcedure1.StoredProcedure;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -14,12 +13,6 @@ namespace StoredProcedure1
         static void Main(string[] args)
         {
             Database db = new Database();
-            sp_ÖgrenciDers_SELECT ogr_select = new sp_ÖgrenciDers_SELECT();
-            sp_ÖgrenciDers_INSERT ogr_insert = new sp_ÖgrenciDers_INSERT();
-            ogr_insert.ogr_id = 1;
-            ogr_insert.ders_kodu = "4";
-            ogr_select.OGR_ID = 1;
-            db.getData(ogr_select);
             Console.ReadKey();
         }
     }
