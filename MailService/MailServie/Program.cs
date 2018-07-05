c10c4ccef333dda7f6e77da2f2946497b1ab8e46
Ömer Faruk Şahin <faruk_thecno@hotmail.com>
2018-07-05 08:55:39 +0300

    VS-GrupArge

---
+++ MailService/MailServie/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailServie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailMessage message = new MailMessage();
+            message.Subject = "Mail Service Test";
+            message.From = new MailAddress("sender");
+            message.Body = "Mail Service Test";
+            message.To.Add("receiver");
+
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+            smtp.Credentials = new NetworkCredential("mail", "password");
+            smtp.EnableSsl = true;
+            smtp.Port = 587;
+            smtp.Send(message);
+            Console.ReadKey();
+        }
+    }
+}
