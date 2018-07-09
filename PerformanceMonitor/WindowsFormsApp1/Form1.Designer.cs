ef98612316806649e49505a29dec544b356d103b
Ömer Faruk Şahin <faruk_thecno@hotmail.com>
2018-07-09 10:51:43 +0300

    VS-GrupArge

---
+++ PerformanceMonitor/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.performanceCounter1 = new System.Diagnostics.PerformanceCounter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 209);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(352, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(12, 257);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(352, 23);
+            this.progressBar2.TabIndex = 1;
+            // 
+            // performanceCounter1
+            // 
+            this.performanceCounter1.CategoryName = "Processor Information";
+            this.performanceCounter1.CounterName = "% Processor Time";
+            this.performanceCounter1.InstanceName = "_Total";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 235);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 309);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(351, 186);
+            this.listBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 283);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 331);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Diagnostics.PerformanceCounter performanceCounter1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PerformanceMonitor/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region Variables
+        private Timer timer;
+        private PerformanceCounter performanceCounter3;
+        private PerformanceCounter performanceCounter4;
+        private string processName;
+        private StreamWriter sw;
+        #endregion
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer = new Timer();
+            timer.Interval = 5000;
+            timer.Start();
+            timer.Tick += Timer_Tick;
+            this.FormClosed += Form1_FormClosed;
+            foreach (var item in Process.GetProcesses().OrderBy(x => x.ProcessName))
+            {
+                listBox1.Items.Add(item.ProcessName);
+            }
+            listBox1.SelectedIndexChanged += ListBox1_SelectedIndexChanged;
+          
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //PROCESS VERİLERİNİ TAKİP ETME
+            processName = listBox1.SelectedItem.ToString();
+            performanceCounter3 = new PerformanceCounter("Process", "% Processor Time", processName);
+            performanceCounter4 = new PerformanceCounter("Process", "Working Set - Private", processName);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                //LOG DİZİNİ OLUŞTURMA
+                var logPath = Directory.GetCurrentDirectory() + "/log".ToString();
+                if (!File.Exists(logPath))
+                {
+                    Directory.CreateDirectory(logPath);
+                }
+                progressBar1.Value = (int)performanceCounter1.NextValue();
+                label1.Text = progressBar1.Value.ToString();
+                if (performanceCounter3 != null)
+                {
+                    //HAM VERİYİ OKUMA
+                    var process_ram = performanceCounter4.NextValue(); // Seçili Process Ram Kullanımı
+                    var value = performanceCounter3.NextValue() / Environment.ProcessorCount;
+                    progressBar2.Value = (int)value;
+                    label3.Text = value.ToString();
+                    label2.Text = "RAM:" + (((int)process_ram / (int)1024) / 1024).ToString() + "MB";
+
+                    //LOGLAMA
+                    sw = new StreamWriter(logPath+"/"+DateTime.Now.ToShortDateString()+".txt",true);
+                    sw.WriteLine("TOTAL CPU USAGE:%"+(int)performanceCounter1.NextValue());
+                    sw.WriteLine(processName+" CPU USAGE:%"+value);
+                    sw.WriteLine(processName+" RAM USAGE:"+ (((int)process_ram / (int)1024) / 1024).ToString() + "MB");
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"HATA",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                (sender as Timer).Dispose();
+            }
+            
+        }
+
+      
+    }
+}
