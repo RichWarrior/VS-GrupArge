c97f6cf6a11f0b9bda42dad53a450223a4894b76
Ömer Faruk Şahin <faruk_thecno@hotmail.com>
2018-07-18 15:19:53 +0300

    VS-GrupArge
    TCPListener and Logs Parse

---
+++ TCPClient/LogFile/Program.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LogFile
+{
+    class Program
+    {
+        public class StateObject
+        {
+            // Client socket.  
+            public Socket workSocket = null;
+            // Size of receive buffer.  
+            public const int BufferSize = 256;
+            // Receive buffer.  
+            public byte[] buffer = new byte[BufferSize];
+            // Received data string.  
+            public StringBuilder sb = new StringBuilder();
+        }
+
+        public class AsynchronousClient
+        {
+            // The port number for the remote device.  
+            public const int port = 11000;
+
+            // ManualResetEvent instances signal completion.  
+            public static ManualResetEvent connectDone =
+                new ManualResetEvent(false);
+            public static ManualResetEvent sendDone =
+                new ManualResetEvent(false);
+            public static ManualResetEvent receiveDone =
+                new ManualResetEvent(false);
+
+            // The response from the remote device.  
+            public static String response = String.Empty;
+
+            public static void StartClient()
+            {
+                // Connect to a remote device.  
+                try
+                {
+
+                    IPHostEntry ipHostInfo = Dns.GetHostEntry("192.168.1.41");
+                    IPAddress ipAddress = ipHostInfo.AddressList[1];
+                    IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+                    // Create a TCP/IP socket.  
+                    Socket client = new Socket(ipAddress.AddressFamily,
+                        SocketType.Stream, ProtocolType.Tcp);
+
+                    // Connect to the remote endpoint.  
+                    client.BeginConnect(remoteEP,
+                        new AsyncCallback(ConnectCallback), client);
+                    connectDone.WaitOne();
+
+                    // Send test data to the remote device.  
+                    sendDone.WaitOne();
+
+                    // Receive the response from the remote device.  
+                    Receive(client);
+                    receiveDone.WaitOne();
+
+                    // Write the response to the console.  
+                    Console.WriteLine("Response received : {0}", response);
+                    // Release the socket.  
+                    client.Shutdown(SocketShutdown.Both);
+                    client.Close();
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+
+            public static void ConnectCallback(IAsyncResult ar)
+            {
+                try
+                {
+                    // Retrieve the socket from the state object.  
+                    Socket client = (Socket)ar.AsyncState;
+
+                    // Complete the connection.  
+                    client.EndConnect(ar);
+
+                    Console.WriteLine("Socket connected to {0}",
+                        client.RemoteEndPoint.ToString());
+
+                    // Signal that the connection has been made.  
+                    connectDone.Set();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+
+            public static void Receive(Socket client)
+            {
+                try
+                {
+                    // Create the state object.  
+                    StateObject state = new StateObject();
+                    state.workSocket = client;
+
+                    // Begin receiving the data from the remote device.  
+                    client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                        new AsyncCallback(ReceiveCallback), state);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+
+            public static void ReceiveCallback(IAsyncResult ar)
+            {
+                try
+                {
+                    // Retrieve the state object and the client socket   
+                    // from the asynchronous state object.  
+                    StateObject state = (StateObject)ar.AsyncState;
+                    Socket client = state.workSocket;
+
+                    // Read data from the remote device.  
+                    int bytesRead = client.EndReceive(ar);
+
+                    if (bytesRead > 0)
+                    {
+                        // There might be more data, so store the data received so far.  
+                        state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+
+                        // Get the rest of the data.  
+                        client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                            new AsyncCallback(ReceiveCallback), state);
+                    }
+                    else
+                    {
+                        // All the data has arrived; put it in response.  
+                        if (state.sb.Length > 1)
+                        {
+                            response = state.sb.ToString();
+                        }
+                        // Signal that all bytes have been received.  
+                        receiveDone.Set();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+
+            public static void Send(Socket client, String data)
+            {
+                // Convert the string data to byte data using ASCII encoding.  
+                byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+                // Begin sending the data to the remote device.  
+                client.BeginSend(byteData, 0, byteData.Length, 0,
+                    new AsyncCallback(SendCallback), client);
+            }
+
+            public static void SendCallback(IAsyncResult ar)
+            {
+                try
+                {
+                    // Retrieve the socket from the state object.  
+                    Socket client = (Socket)ar.AsyncState;
+
+                    // Complete sending the data to the remote device.  
+                    int bytesSent = client.EndSend(ar);
+                    Console.WriteLine("Sent {0} bytes to server.", bytesSent);
+
+                    // Signal that all bytes have been sent.  
+                    sendDone.Set();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+            static void Main(string[] args)
+            {
+                IPHostEntry ipHostInfo = Dns.GetHostEntry("192.168.1.41");
+                IPAddress ipAddress = ipHostInfo.AddressList[1];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+                // Create a TCP/IP socket.  
+                Socket client = new Socket(ipAddress.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                // Connect to the remote endpoint.  
+                client.BeginConnect(remoteEP,
+                    new AsyncCallback(ConnectCallback), client);
+
+                string fpath = @"C:\Users\ŞENCAN\Desktop\ModemLogs";
+                var directory = Directory.GetDirectories(fpath, "*", SearchOption.AllDirectories);
+                foreach (var item in directory)
+                {
+                    foreach (var sub in Directory.GetFiles(item.ToString()))
+                    {
+                        fpath = sub.ToString();
+                        using (StreamReader reader = new StreamReader(fpath))
+                        {
+                            //StartClient();
+                            string file = reader.ReadToEnd();
+                            file = file.Replace("\r", "").Replace("\n", "");
+                            string[] s = new string[] { "C: " };
+                            var dizi = file.Split(s, StringSplitOptions.None);
+                            //List<string> list = new List<string>();
+                            foreach (var dizi_item in dizi)
+                            {
+                                if (dizi_item.Contains("S: "))
+                                {
+
+                                    var y = dizi_item.Split(new string[] { "S: " }, StringSplitOptions.None);
+                                    //list.Add(y[0].Remove(y[0].Length - 9, 9));
+                                    var z = "<" + y[0].Remove(y[0].Length - 9, 9) + ">";
+                                    Send(client, z);
+                                }
+                            }
+                        }
+                    }
+                }
+                connectDone.WaitOne();
+
+                // Send test data to the remote device.  
+                sendDone.WaitOne();
+
+                // Receive the response from the remote device.  
+                Receive(client);
+                receiveDone.WaitOne();
+
+                // Write the response to the console.  
+                Console.WriteLine("Response received : {0}", response);
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+                //string fpath = @"C:\Users\ŞENCAN\Desktop\ModemLogs\GRP22078\GRP22078_20180718.log";
+                //using (StreamReader reader = new StreamReader(fpath))
+                //{
+                //    string file = reader.ReadToEnd();
+                //    file = file.Replace("\r", "").Replace("\n", "");
+                //    string[] s = new string[] { "C: " };
+                //    var dizi = file.Split(s, StringSplitOptions.None);
+                //    List<string> list = new List<string>();
+                //    foreach (var dizi_item in dizi)
+                //    {
+                //        if (dizi_item.Contains("S: "))
+                //        {
+
+                //            var y = dizi_item.Split(new string[] { "S: " }, StringSplitOptions.None);
+                //            list.Add(y[0].Remove(y[0].Length - 9, 9));
+                //            var z = "<" + y[0].Remove(y[0].Length - 9, 9) + ">";
+                //            Console.WriteLine(z);
+                //        }
+                //    }
+                //}
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
+
+
